4ef132f4175a674911529f07d7ba411f6747a3ec
Aryan Kedar <your-email@example.com>
2026-02-04 20:09:42 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,218 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import Logo from './assets/work_logo/Rock.png';
+import Logo1 from './assets/work_logo/ecommerce.png';
+import ProgressBar from './assets/work_logo/progressbar.png';
+import IT from "./assets/IT_VEDANT.jpg";
+import Rapid from "./assets/Rapid.jpeg";
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: Rapid,
+      role: "Data Analyst",
+      company: "Rapid SoftLogic Private Limited",
+      date: "May 2025 - November 2025",
+      desc: "Analyzed real-world datasets and created interactive dashboards using SQL,Power BI,Python and Excel",
+      skills: [
+        "SQL",
+        "Power BI",
+        "Python",
+        "Excel",
+        "Machine Learning"
+      ],
+    },
+    {
+      id: 1,
+      img: IT,
+      role: "Java Fullstack Developer Intern",
+      company: "IT VEDANT",
+      date: "NOV 2025 - Present",
+      desc: "Worked on multiple projects involving Java, Spring Boot for backend development, and React.js for frontend development. Gained hands-on experience in building full-stack applications, implementing RESTful APIs, and managing databases.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Java",
+        "Material UI",
+        "Spring Boot",
+        "React JS",
+        "MySQL",
+        "Git",
+        "Spring",
+        "PostMan"
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Shri Ramdeobaba College of Engineering and Management",
+      date: "Nov 2021 - May 2025",
+      grade: "8.02 CGPA",
+      desc: "I have completed my Bachelors of Technology (B.Tech) in Electronics And Communication from Ramdeobaba University, Nagpur. During my time at RCOEM, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelors Of Technology - B.Tech (Electronics and Communication)",
+    },
+    
+      
+    {
+      id: 1,
+      img: vpsLogo,
+      school: "Nirala College, Nagpur",
+      date: "Apr 2019 - March 2021",
+      grade: "78%",
+      desc: "I completed my class 12 education from Nirala College, Nagpur, under the State board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Lalitha Public School,Nagpur",
+      date: "Apr 2006 - March 2018",
+      grade: "89.4%",
+      desc: "I completed my class 10 education from Lalitha Public School,Nagpur, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Rock Paper Scissors Game",
+      description:
+        "A web-based Rock Paper Scissors game built using React JS that allows users to play against the computer. The game features a user-friendly interface, score tracking, and responsive design for an engaging gaming experience.",
+      image: Logo,
+      tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+      webapp: "https://aryan1234725.github.io/TIC-TAC-TOE/",
+    },
+    {
+      id: 1,
+      title: "E-COMMERECE WEBSITE",
+      description:
+        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+      image: Logo1,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Aryan1234725/E-COMMERCE_REACT_MAIN",
+    },
+    {
+      id: 2,
+      title: "PROGERESS BAR",
+      description:
+        "A Javascript based Progress bar which give progress at every stage also give percentage progress at each level.",
+      image: ProgressBar,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://progressbar-olive.vercel.app/",
+    },
+  ];  
